ff3bed28e884d4f7743d4a5e66490670e6c88c9a
KiberK0tleta <filippovi@gmail.com>
2021-06-07 12:10:42 +0400

    Add project files.

---
+++ ForwardList/Main.cpp
@@ -0,0 +1,140 @@
+﻿#include<iostream>
+using namespace std;
+#define tab "\t"
+
+class Element
+{
+	int Data;           //Значение элемента
+	Element* pNext;    //Указатель на следующий элемент
+	static int count;
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		count++;
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		count--;
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class FarwardList;
+};
+int Element::count = 0; //Инициализация стат переменной
+
+class FarwardList
+{
+	Element* Head;
+	int size;
+public:
+	FarwardList()
+	{
+		Head = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~FarwardList()
+	{
+		
+		cout << "LDestructor:\t" << this << endl;
+	}
+	//---------------------------------------------//
+	void push_front(int Data)
+	{
+		Element* New = new Element(Data);
+		New->pNext = Head;
+		Head = New;
+		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)
+		{
+			push_front(Data);
+			return;
+		}
+		Element* New = new Element(Data);
+		Element* Temp = Head;
+		while (Temp->pNext != nullptr)
+		{
+			Temp = Temp->pNext;
+		}
+		Temp->pNext = New;
+		size++;
+	}
+	void pop_front()
+	{
+		Element* Temp = Head;
+		Head = Head->pNext;
+		delete Temp;
+		size--;
+	}
+	void pop_back()
+	{
+		Element* Temp = Head;
+		while (Temp->pNext->pNext != nullptr)
+		{
+			Temp = Temp->pNext;
+		}
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+		size--;
+	}
+	void insert(int Data, int index)
+	{
+		if (index == 0)
+		{
+			push_front(Data);
+			return;
+		}
+		Element* New = new Element(Data);
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			Temp = Temp->pNext;
+			if (Temp->pNext == nullptr)break;
+		}
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+		size++;
+	}
+	void print()
+	{
+		Element* Temp = Head;
+		while (Temp != nullptr)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;
+		}
+		cout << "Кол-во эл в списку: " << size << endl;
+		cout << "Общее Кол-во эл в списку: " << Element::count << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Размер списка: "; cin >> n;
+	FarwardList list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_back(rand() % 100);
+	}
+	list.print();
+
+	int value;
+	int index;
+	cout << "value:"; cin >> value;
+	cout << "index: "; cin >> index;
+	list.insert(value, index);
+	list.print();
+
+	cout << "list2" << endl;
+	FarwardList list2;
+	list2.push_back(3);
+	list2.push_back(5);
+	list2.push_back(8);
+	list2.print();
+		
+}
